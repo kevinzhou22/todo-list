ff0dbbab154a3f3cc4d39783ce73c8867442945a
Kevin Zhou <kevinzhou22@outlook.com>
2020-12-28 19:56:40 +1100

    Add basic pubsub module

---
+++ src/modules/events.js
@@ -0,0 +1,27 @@
+const events = {};
+
+function on(eventName, fn) {
+    events[eventName] = events[eventName] || [];
+    events[eventName].push(fn);
+}
+
+function off(eventName, fn) {
+    if (events[eventName]) {
+        for (let i = 0; i < events[eventName].length; i++) {
+            if (events[eventName][i] === fn) {
+                events[eventName].splice(i,1)
+                break;
+            }
+        }
+    }
+}
+
+function emit(eventName, data) {
+    if (events[eventName]) {
+        events[eventName].forEach(function(fn) {
+            fn(data);
+        });
+    }
+}
+
+export {on, off, emit};
